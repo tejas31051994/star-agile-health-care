76734b457b751cc2effe9236cc4fc10f69d95525
tejas31051994 <tejtejas.sawant@gmail.com>
2024-10-06 12:41:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,17 @@
 locals {
-  region - "ap-south-1"  
-  name   - "${terraform.workspace}-cluster"
-  vpc_cidr - "10.123.0.0/16"
-  azs      - ["ap-south-1a","ap-south-1b"
-  public_subnets  - ["10.123.1.0/24","10.123.2.0/24"]
-  private_subnets - ["10.123.3.0/24","10.123.4.0/24"]
-  intra_subnets   - ["10.123.5.0/24","10.123.6.0/24"]
-  tags - {
+  region = "ap-south-1"  
+  name   = "${terraform.workspace}-cluster"
+  vpc_cidr = "10.123.0.0/16"
+  azs      = ["ap-south-1a","ap-south-1b"
+  public_subnets  = ["10.123.1.0/24","10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24","10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24","10.123.6.0/24"]
+  tags = {
     Example = local.name
    } 
   }
 
   provider "aws" {
-    region - "ap-south-1"
+    region = "ap-south-1"
  }
    
